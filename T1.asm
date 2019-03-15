5fbd4dda29b0f67ecbf8197fdcd63a809fd5e440
Vakihito <victor.kamada.tomita@gmail.com>
2019-03-15 16:17:46 -0300

    "adionado as funcoes de icm 			raiz quadrada 			e tabuada"

--- T1.asm
+++ T1.asm
@@ -1,17 +1,23 @@
 .data
 .align 0
 	 str_digi : .asciiz "digite qual operacao voce quer :  "
-	 str_menu : .asciiz " 1 - soma\n 2 - subtracao\n 3 - multiplicacao\n 4 - divisao \n 5 - potenciacao \n\n"
+	 str_menu : .asciiz " 1 - soma\n 2 - subtracao\n 3 - multiplicacao\n 4 - divisao \n 5 - potenciacao \n 6 - raiz quadrada \n 7 - tabuada \n 8 - ICM \n\n"
 	 str_nume : .asciiz "\nentre com um numero: "
 	 str_qpot : .asciiz "\na ser elevado a qual potencia:"
+	 str_peso : .asciiz "\nentre com o peso da pessoa:"
+	 str_altu : .asciiz "\nentre com a altura da pessoa:"
+	 str_espa : .asciiz "\n"
 	 
-	 str_soma : .asciiz "o resultado da soma entre os dois numeros eh: "
-	 str_subi : .asciiz "o resultado da subtracao entre os dois numeros eh: "
-	 str_mult : .asciiz "o resultado da multiplicacao entre os dois numeros eh: "
-	 str_quoc : .asciiz "o quociente da divisao entre os dois numeros eh: "
+	 str_soma : .asciiz "o resultado da soma entre os dois numeros eh : "
+	 str_subi : .asciiz "o resultado da subtracao entre os dois numeros eh : "
+	 str_mult : .asciiz "o resultado da multiplicacao entre os dois numeros eh : "
+	 str_quoc : .asciiz "o quociente da divisao entre os dois numeros eh : "
 	 str_rest : .asciiz "\no resto da divisao eh :  "
-	 str_pote : .asciiz "\na potencia desse numero eh:  "
-
+	 str_pote : .asciiz "\na potencia desse numero eh :  "
+	 str_raiz : .asciiz "\na raiz quadra desse numero eh : " 
+	 str_nrai : .asciiz "\no numero nao apresenta raiz inteira"
+	 str_tabu : .asciiz "\na tabuada do numero apresentado eh: "
+	 str_imcc : .asciiz "\no imcc da pessoa eh :  "
 
 .text
 .globl main
@@ -60,8 +66,21 @@
 			jal Potencia
 			j the_end
 		not_pote:
-			
-		
+			li $t1, 6
+			bgt $t0, $t1, not_raiz
+			jal raiz
+			j the_end
+		not_raiz:
+			li $t1, 7
+			bgt $t0, $t1, not_tabu
+			jal tabuada
+			j the_end
+		not_tabu:
+			li $t1, 8
+			bgt $t0, $t1, not_imcc
+			jal imc
+			j the_end
+		not_imcc:
 		
 		
 		the_end:
@@ -289,8 +308,151 @@
 				addi $sp, $sp, 8
 				jr $ra
 		
+		raiz : 
+			addi $sp, $sp, -8 #alocando o espaço de necessa
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
+			
+			li $v0, 4
+			la $a0, str_nume #solicitando numero de entrada para a 
+					 # fazer a raiz quadrada
+			syscall
+			
+			li $v0, 5  
+			syscall		#repassando para $t2
+			move $t2, $v0
+			
+			li $t3, 1 #repassando a menor variavel para q seja raiz
+			
+			
+			loop_raiz:
+				mul $t4, $t3, $t3 	# $t4 é $t3 ao qudrado
+				beq $t4, $t2, end_craiz # se $t4 for igual a 
+						       	# $t2 entao $t3 e a raiz
+						       	
+				bgt $t4, $t2, end_sraiz # se $t4 > $t2
+							# nao tem raiz
+				addi $t3, $t3, 1 
+				j loop_raiz
+			
+			
+			end_craiz: 
+				
+				li $v0, 4
+				la $a0, str_raiz # a raiz o numero eh:
+				syscall
+			
+				li $v0, 1
+				move $a0, $t3  	  # print raiz
+				syscall
+				j end_raiz_f
+				
+			end_sraiz:
+				li $v0, 4
+				la $a0, str_nrai # a sem raiz
+				syscall
+			
+			end_raiz_f:
 		
+				lw $a0, 0($sp)
+				lw $ra, 4($sp)
+				addi $sp, $sp, 8
+				jr $ra
+		tabuada: 
+			addi $sp, $sp, -8 #alocando o espaço de necessa
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
+			
+			li $v0, 4
+			la $a0, str_nume #solicitando numero de entrada para a 
+					
+			syscall
+			
+			li $v0, 5  
+			syscall		#repassando para $t2
+			move $t2, $v0
+			
+			li $t3, 0  # contador
+			li $t4, 10 # valor constante q determinada fim do loop
+			
+			li $v0, 4
+			la $a0, str_tabu #imprime tabuada
+			syscall
+			
+			
+			loop_tabu:
+				
+						  #multiplica valor de entrada
+						  #pelo contador
+				mul $t5, $t2, $t3 # $t5 valor a ser
+						  # printado da tabuada
+						  # variavel auxiliar
+				
+				li $v0, 4
+				la $a0, str_espa #imprime espaço
+				syscall
+				
+				li $v0, 1
+				move $a0, $t5  	  # a tabuada
+				syscall
+				
+				addi $t3, $t3, 1 
+				
+				bgt $t3, $t4, end_tabu 	#verifica se o contador 
+						    	# eh maior q "10"
+				
+				  
+				j loop_tabu
+			
+			
+			
+				
+			
+			
+			end_tabu:
 		
+				lw $a0, 0($sp)
+				lw $ra, 4($sp)
+				addi $sp, $sp, 8
+				jr $ra
+		imc: 
+			addi $sp, $sp, -8 #alocando o espaço de necessa
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
+			
+			li $v0, 4
+			la $a0, str_peso #solicitando peso	 
+			syscall
+			
+			li $v0, 6  	#valor fica em $f0
+			syscall		 
+			mov.s $f1, $f0	#repassa para $f1
+			
+			li $v0, 4
+			la $a0, str_altu #solicitando pela altura					
+			syscall
+			
+			li $v0, 6  
+			syscall		
+			mov.s $f2, $f0	#repassando a altura para $f2
+			
+			mul.s $f2, $f2, $f2 # elevando a altura ao quadrado
+			div.s $f1, $f1, $f2 # f0 rescebe a divisao entre 
+					    # f0 e f1  
+			
+			li $v0, 4
+			la $a0, str_imcc
+			syscall
+			
+			li $v0, 2
+			mov.s $f12, $f1
+			syscall
+			
+			
+			lw $a0, 0($sp)
+			lw $ra, 4($sp)
+			addi $sp, $sp, 8
+			jr $ra
 		
 		
 		
