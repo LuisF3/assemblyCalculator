1784389ac0219779f664fb9af84590a3676c2267
Vakihito <victor.kamada.tomita@gmail.com>
2019-03-16 02:37:21 -0300

    "trabalho com todos os seviços exigidos "

--- T1.asm
+++ T1.asm
@@ -1,12 +1,15 @@
 .data
 .align 0
 	 str_digi : .asciiz "digite qual operacao voce quer :  "
-	 str_menu : .asciiz " 1 - soma\n 2 - subtracao\n 3 - multiplicacao\n 4 - divisao \n 5 - potenciacao \n 6 - raiz quadrada \n 7 - tabuada \n 8 - ICM \n\n"
+	 str_menu : .asciiz " 1 - soma\n 2 - subtracao\n 3 - multiplicacao\n 4 - divisao \n 5 - potenciacao \n 6 - raiz quadrada \n 7 - tabuada \n 8 - ICM \n 9 - Fatorial \n 10 - fibonacci \n 11 - Encerrar \n\n"
 	 str_nume : .asciiz "\nentre com um numero: "
 	 str_qpot : .asciiz "\na ser elevado a qual potencia:"
 	 str_peso : .asciiz "\nentre com o peso da pessoa:"
 	 str_altu : .asciiz "\nentre com a altura da pessoa:"
 	 str_espa : .asciiz "\n"
+	 str_povi : .asciiz " ; "
+	 str_fiin : .asciiz "\nentre com o limite inferior : "
+	 str_fisu : .asciiz "\nentre com o limite superior : "
 	 
 	 str_soma : .asciiz "o resultado da soma entre os dois numeros eh : "
 	 str_subi : .asciiz "o resultado da subtracao entre os dois numeros eh : "
@@ -17,7 +20,11 @@
 	 str_raiz : .asciiz "\na raiz quadra desse numero eh : " 
 	 str_nrai : .asciiz "\no numero nao apresenta raiz inteira"
 	 str_tabu : .asciiz "\na tabuada do numero apresentado eh: "
-	 str_imcc : .asciiz "\no imcc da pessoa eh :  "
+	 str_imcc : .asciiz "\no imc da pessoa eh :  "
+	 str_fato : .asciiz "\no fatoial do numero apresentado eh :  "
+	 str_fibi : .asciiz "\no fibonacci no intervalo apresentada eh: "
+	 
+	 
 
 .text
 .globl main
@@ -81,8 +88,16 @@
 			jal imc
 			j the_end
 		not_imcc:
-		
-		
+			li $t1, 9
+			bgt $t0, $t1, not_fato
+			jal fatorial
+			j the_end
+		not_fato:
+			li $t1, 10
+			bgt $t0, $t1, the_end
+			jal fibonacci
+			j the_end
+	
 		the_end:
 			li $v0, 10
 			syscall
@@ -454,8 +469,141 @@
 			addi $sp, $sp, 8
 			jr $ra
 		
-		
-		
+		fatorial: 
+			addi $sp, $sp, -8 #alocando o espaço de necessa
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
+			
+			li $v0, 4
+			la $a0, str_nume	 
+			syscall
+			
+			li $v0, 5  	
+			syscall		 
+			move $t1, $v0	# numero que devemos fazer o fatorial			
+			move $t2, $v0 	#variavel auxiliar, counter   
+			li $t3, 1	#constante 
+			
+			loop_fat:
+				addi $t2, $t2, -1	# descendo o $t2
+				ble $t2, $t3, end_fat 	# counter < 1 ele sai
+				mul $t1, $t1, $t2	# passando a multiplicacao
+							# armazena em $t1
+				j loop_fat							
+			  
+			end_fat:
+			
+				li $v0, 4
+				la $a0, str_fato
+				syscall
+			
+				li $v0, 1
+				move $a0, $t1
+				syscall
+			
+			
+				lw $a0, 0($sp)
+				lw $ra, 4($sp)
+				addi $sp, $sp, 8
+				jr $ra
+		fibonacci: 
+			addi $sp, $sp, -8 #alocando o espaço de necessa
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
+			
+			li $v0, 4
+			la $a0, str_fiin	 
+			syscall
+			
+			li $v0, 5  	
+			syscall		 
+			move $t1, $v0	#precisa printar entre $t1
+			
+			li $v0, 4
+			la $a0, str_fisu	 
+			syscall
+			
+			li $v0, 5  	
+			syscall		 
+			move $t2, $v0	# e entre $t2
+			
+			#
+ 		#int a = 1;
+		#int b = 1;
+		#int var = 1;
+		#// scanf("%d",&c);
+		#// scanf("%d",&d);
+		#for (int i = 0; var < 100; ++i)
+		#{
+		#	a = a + b;
+		#	var = a;
+		#	printf("%d , ", var);
+		#	b = b + a;
+		#	var = b;
+		##	printf("%d , ", var);
+		#}
+ 			li $t3, 1 # $t3 eh o equivalente a 'a'
+ 			li $t4, 1 # $t4 eh o equivalente a 'b'
+ 			li $t5, 1 # $t5 eh o equivalente a 'var'
+ 			
+ 			li $v0, 4
+			la $a0, str_fibi
+			syscall
+			
+			beq $t1, $t3 ,star_with_one # precisamos fazer primeiro
+						    # eh printar 1 ; 1
+						    # pois o limite inferior eh 1
+ 			
+			loop_fib:
+				add $t3, $t3, $t4 	# a = a + b
+				move $t5 , $t3	 	# var = a
+				bgt $t5, $t2, end_fib 	# se $t5 > $t2 entao termina
+				bge $t5, $t1, print_var # senao, verifica se eh maior 
+							# q o $t1, se for printa
+			part_two:			
+				add $t4, $t3, $t4 	# b = b + a
+				move $t5 , $t4		# var = b
+				bgt $t5, $t2, end_fib	# se $t5 > $t2 entao termina
+				bge $t5, $t1, print_var # senao, verifica se eh maior
+							# q o $t1 e printa
+				j loop_fib
+			print_var:
+				li $v0, 1
+				move $a0, $t5
+				syscall
+					
+				li $v0, 4
+				la $a0, str_povi
+				syscall
+							# a > b , quer dizer a autima coisa
+							# que fizemos foi a = a +b
+				bge $t3 , $t4, part_two #se a > b entao pula para parte 2 se nao
+				j loop_fib		#pula para loop feeb
+						
+			end_fib:
+						
+				lw $a0, 0($sp)
+				lw $ra, 4($sp)
+				addi $sp, $sp, 8
+				jr $ra
+			star_with_one:
+				li $v0, 1
+				move $a0, $t3
+				syscall
+					
+				li $v0, 4
+				la $a0, str_povi
+				syscall
+				
+				li $v0, 1
+				move $a0, $t3
+				syscall
+					
+				li $v0, 4
+				la $a0, str_povi
+				syscall
+				
+				j loop_fib
 		
 		
 		
